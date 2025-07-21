b1cb8aa134858d214e802d3631308d5f0a898452
valad47 <valad.racz@gmail.com>
2025-07-21 12:40:33 +0300

    .

---
+++ access/values.js
@@ -0,0 +1,15 @@
+// Активировать документы (true - включены, false - отключены)
+var isRightsEnabled = false; // Права
+var isCovidCertificateEnabled = false; // COVID-сертификат
+
+// Общее
+var birthdate = "17.03.2007"; // Дата рождения
+var fullname = "Пулько Олександр Максимович" // ФИО
+var name = "Олександр" // Имя
+
+// Паспорт
+var passport_id = "008759371" // Номер паспорта
+
+// КПП
+var kpp_id = "3805401387" // ИНН
+
